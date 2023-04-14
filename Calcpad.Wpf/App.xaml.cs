7bd7b55651eb428b8c2161df1162fac68e9bbf0f
Ned Ganchovski <proektsoft.bg@gmail.com>
2023-04-14 19:09:24 +0300

    Fixed small brackets after pdf export

--- Calcpad.Wpf/App.xaml.cs
+++ Calcpad.Wpf/App.xaml.cs
@@ -13,29 +13,21 @@ namespace Calcpad.Wpf
             if (main.IsSaved)
             {
 #if BG
-                var message =
-                @$"Неочаквана грешка:
-""{GetMessage(ex)}""
-Няма незаписани данни. Calcpad ще се затвори след това съобщение.";
+                var message = GetMessage(ex) +
+                "\r\n\r\nНяма незаписани данни. Calcpad ще се затвори след това съобщение.";
 #else
-                var message =
-                @$"Unexpected error:
-""{GetMessage(ex)}""
-There is no unsaved data. Calcpad will shut down after this message.";
+                var message = GetMessage(ex) +
+                "\r\n\r\nThere is no unsaved data. Calcpad will shut down after this message.";
 #endif
                 MessageBox.Show(message, "Calcpad", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 #if BG
-                var message1 =
-                @$"Неочаквана грешка:
-""{GetMessage(ex)}""
-Calcpad ще запази вашите данни и ще се опита да се възстанови автоматчно.";
+            var message1 = GetMessage(ex) +
+            "\r\n\r\nCalcpad ще запази вашите данни и ще се опита да се възстанови автоматчно.";
 #else
-            var message1 =
-            @$"Unexpected error:
-""{GetMessage(ex)}""
-Calcpad will save your work and will try to recover automatically.";
+            var message1 = GetMessage(ex) +
+            "\r\n\r\nCalcpad will save your work and will try to recover automatically.";
 #endif
             e.Handled = true;
             try
@@ -48,24 +40,29 @@ Calcpad will save your work and will try to recover automatically.";
             catch (Exception ex2)
             {
 #if BG
-                var message2 =
-                @$"Неочаквана грешка:
-""{GetMessage(ex2)}""
-Възстановяването е неуспешно. Ако проблемът продължи, моля пишете на proektsoft.bg@gmail.com.";
+                var message2 = GetMessage(ex2) +
+                "\r\n\r\nВъзстановяването е неуспешно. Ако проблемът продължи, моля пишете на proektsoft.bg@gmail.com.";
 #else
-                var message2 =
-                @$"Unexpected error:
-""{GetMessage(ex2)}""
-Unable to recover. If the problem persists, please contact us on proektsoft.bg@gmail.com.";
+                var message2 = GetMessage(ex2) +
+                "\r\n\r\nUnable to recover. If the problem persists, please contact us on proektsoft.bg@gmail.com.";
 #endif
                 MessageBox.Show(message2, "Calcpad", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-
-        private static string GetMessage(Exception e) => @$"Unexpected error:
+#if BG
+        private static string GetMessage(Exception e) => 
+@$"Неочаквана грешка:
 ""{e.Message}""
-""Source: {e.Source}""
-""Data: {e.Data}""
-""Stack Trace: {e.StackTrace}""";
+
+Източник: 
+""{e.Source}""";
+#else
+        private static string GetMessage(Exception e) =>
+@$"Unexpected error:
+""{e.Message}""
+
+Source: 
+""{e.Source}""";
+#endif
     }
 }
