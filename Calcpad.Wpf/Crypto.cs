8488a2c711f5b46bbdaf366167619cb5c6cdec17
Ned Ganchovski <proektsoft.bg@gmail.com>
2023-10-24 12:15:50 +0300

    Improved cpdz packing
    Modules and local images are packed into the cpdz file.
    Source code is encrypted.

---
+++ Calcpad.Wpf/Crypto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Calcpad.Wpf
+{
+    internal static class Crypto
+    {
+        private const string _key = "Proekt$OFT123456"; //Set your own key here (16 bytes)
+        public static string EncryptString(string s)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(_key);
+                aesAlg.IV = new byte[16]; // Initialization Vector (IV)
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(s);
+                        }
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string DecryptString(string s)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(_key);
+                aesAlg.IV = new byte[16]; // Initialization Vector (IV)
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(s)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Calcpad.Wpf/zip.cs
+++ Calcpad.Wpf/zip.cs
@@ -2,10 +2,11 @@
 using System.IO;
 using System.IO.Compression;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Calcpad.Wpf
 {
-    internal class Zip
+    internal static class Zip
     {
         internal static void Compress(string text, Stream fs)
         {
@@ -16,6 +17,63 @@ namespace Calcpad.Wpf
             ms.Position = 0;
             using var ds = new DeflateStream(fs, CompressionMode.Compress);
             ms.CopyTo(ds);
+        }
+
+        internal static void CompressWithImages(string text, string[] images, string fileName)
+        {
+            using (FileStream zipStream = new FileStream(fileName, FileMode.Create))
+            {
+                using (ZipArchive archive = new ZipArchive(zipStream, ZipArchiveMode.Create))
+                {
+                    var textFile = Path.GetTempFileName();
+                    File.WriteAllText(textFile, Crypto.EncryptString(text));
+                    ZipArchiveEntry textEntry = archive.CreateEntry(Path.GetFileName(textFile));
+                    using (Stream entryStream = textEntry.Open())
+                    {
+                        using (var fileStream = File.OpenRead(textFile))
+                        {
+                            fileStream.CopyTo(entryStream);
+                        }
+                    }
+                    if (images is null)
+                        return;
+                    
+                    var sourcePath = Path.GetDirectoryName(fileName);
+                    var sourceParent = Directory.GetDirectoryRoot(sourcePath);
+                    if (!string.Equals(sourceParent, sourcePath, StringComparison.OrdinalIgnoreCase))
+                        sourceParent = Directory.GetParent(sourcePath).FullName;
+
+                    var regexString = @"src\s*=\s*""\s*\.\./";
+                    for (int i = 0; i < 2; ++i)
+                    {
+                        foreach (var image in images)
+                        {
+                            var m = Regex.Match(image, regexString, RegexOptions.IgnoreCase);
+                            if (m.Success)
+                            {
+                                var n = m.Length;
+                                var imageFileName = image[n..^1].Replace('/', '\\');
+                                var imageFilePath = Path.Combine(sourceParent, imageFileName);
+                                if (File.Exists(imageFilePath))
+                                {
+                                    ZipArchiveEntry imageEntry = archive.CreateEntry(imageFileName);
+                                    using (Stream entryStream = imageEntry.Open())
+                                    {
+                                        using (FileStream fileStream = File.OpenRead(imageFilePath))
+                                        {
+                                            fileStream.CopyTo(entryStream);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        regexString = @"src\s*=\s*""\s*\./";
+                        if (string.Equals(sourceParent, sourcePath, StringComparison.OrdinalIgnoreCase))
+                            return;
+                        sourceParent = sourcePath;
+                    }
+                }
+            }
         }
 
         internal static SpanLineEnumerator Decompress(Stream fs) =>
@@ -29,6 +87,44 @@ namespace Calcpad.Wpf
             ms.Position = 0;
             using var sr = new StreamReader(ms);
             return sr.ReadToEnd();
+        }
+
+        internal static bool IsComposite(string fileName)
+        {
+            byte[] signature = new byte[] { 0x50, 0x4B }; // Signature for ZIP files
+
+            using (FileStream fileStream = File.OpenRead(fileName))
+            {
+                byte[] fileSignature = new byte[2];
+                fileStream.Read(fileSignature, 0, 2);
+                return fileSignature[0] == signature[0] && fileSignature[1] == signature[1];
+            }
+        }
+
+        internal static string DecompressWithImages(string fileName)
+        {
+            var filePath = Path.GetDirectoryName(fileName);
+            string text = string.Empty;
+            using (ZipArchive archive = ZipFile.OpenRead(fileName))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    string entryPath = Path.Combine(filePath, entry.FullName);
+                    Directory.CreateDirectory(Path.GetDirectoryName(entryPath));
+                    if (entry.Length == 0) // It's a directory
+                        Directory.CreateDirectory(entryPath);
+                    else // It's a file
+                    {
+                        entry.ExtractToFile(entryPath, true);
+                        if (string.Equals(Path.GetExtension(entry.Name), ".tmp", StringComparison.OrdinalIgnoreCase))
+                        {
+                            text = Crypto.DecryptString(File.ReadAllText(entryPath));
+                            File.Delete(entryPath);
+                        }
+                    }
+                }
+            }
+            return text;
         }
     }
 }
