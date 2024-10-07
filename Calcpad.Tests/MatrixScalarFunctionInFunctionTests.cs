73e497679f82bdc1c47d335e13b71fda62c73c7c
Ned Ganchovski <proektsoft.bg@gmail.com>
2024-10-07 17:41:44 +0300

    Fixed take function, issue #368

--- Calcpad.Tests/MatrixScalarFunctionInFunctionTests.cs
+++ Calcpad.Tests/MatrixScalarFunctionInFunctionTests.cs
@@ -1,13 +1,4 @@
-﻿using SkiaSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Runtime.Intrinsics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calcpad.Tests
+﻿namespace Calcpad.Tests
 {
     public class MatrixScalarFunctionInFunctionTests
     {
