d6ccd42ca33716cdccabd58fa38921da900b9c63
Ned Ganchovski <proektsoft.bg@gmail.com>
2024-12-27 13:33:11 +0200

    Updated to version 7.1.6

--- Calcpad.Wpf/MainWindow.xaml.cs
+++ Calcpad.Wpf/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace Calcpad.Wpf
     public partial class MainWindow : Window
     {
         //Culture
-        private static readonly string _currentCultureName = "en";
+        private static readonly string _currentCultureName = "bg";
 
         //Static resources
         private static readonly char[] GreekLetters = ['α', 'β', 'χ', 'δ', 'ε', 'φ', 'γ', 'η', 'ι', 'ø', 'κ', 'λ', 'μ', 'ν', 'ο', 'π', 'θ', 'ρ', 'σ', 'τ', 'υ', 'ϑ', 'ω', 'ξ', 'ψ', 'ζ'];
